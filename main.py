083056b816b43448f4845b5d36156145d0fc4c27
RiwRiwara <awirut2629@gmail.com>
2024-05-17 02:21:50 +0700

    chore: Refactor code to improve performance and readability

--- main.py
+++ main.py
@@ -13,14 +13,18 @@ templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
 async def home(request: Request):
-    allData = client['iot']['event'].find()
-    allData = list(allData)
-    lastSensorMq7 = client['iot']['event'].find({"sensor_name": "MQ7"}).sort([("created_at", -1)]).limit(1)
-    lastSensorMq7 = list(lastSensorMq7)[0]
-    lastSensorMq9 = client['iot']['event'].find({"sensor_name": "MQ9"}).sort([("created_at", -1)]).limit(1)
-    lastSensorMq9 = list(lastSensorMq9)[0]
+    db = client['iot']['event']
     
-    return templates.TemplateResponse("index.html", {"request": request, "data": allData, "lastSensorMq7": lastSensorMq7, "lastSensorMq9": lastSensorMq9})
+    allData = list(db.find().sort("created_at", -1))
 
+    lastSensorMq7 = db.find_one({"sensor_name": "MQ7"}, sort=[("created_at", -1)])
+    lastSensorMq9 = db.find_one({"sensor_name": "MQ9"}, sort=[("created_at", -1)])
+
+    return templates.TemplateResponse("index.html", {
+        "request": request,
+        "data": allData,
+        "lastSensorMq7": lastSensorMq7,
+        "lastSensorMq9": lastSensorMq9
+    })
 # Mount the sensor API
 app.mount("/sensor", sensor_app)
--- sensorApi.py
+++ sensorApi.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Request
 from fastapi.middleware.cors import CORSMiddleware
 from config.db import client
 from pydantic import BaseModel
@@ -35,28 +35,65 @@ async def main():
 
 @app.get("/db")
 async def testDB():
-    data = client['iot']['event'].find()
+    # sort by created_at
+    data = client['iot']['event'].find().sort("created_at", -1)
     data = list(data)
+    lastSensorMq7 = client['iot']['event'].find_one({"sensor_name": "MQ7"})
+    lastSensorMq9 = client['iot']['event'].find_one({"sensor_name": "MQ9"})
+    
     return {
         "message" : "Connected to MongoDB",
-        "data" : json.loads(json.dumps(data, default=str))
+        "data" : json.loads(json.dumps(data, default=str)),
+        "lastSensorMq7" : json.loads(json.dumps(lastSensorMq7, default=str)),
+        "lastSensorMq9" : json.loads(json.dumps(lastSensorMq9, default=str))
     }
 
 
 
-@app.get("/event/{sensor_name}/{status}/{value}/{description}")
-async def get_event(sensor_name: str, status: str, value: float, description: str):
-    db = client['iot']['event']
+@app.post("/event")
+async def create_event(request: Request):
+    try:
+        data = await request.json()
+        data['created_at'] = datetime.now()
+        data['updated_at'] = datetime.now()
+        
+        print(data)
+        
+        if data['status'] == 'Emergency':
+            print("Emergency")
+        else:
+            result = client['iot']['event'].insert_one(data)
+            if result.inserted_id:
+                return {"message": "Event created successfully"}
+            else:
+                raise HTTPException(status_code=500, detail="Failed to create event")
+                
+        
+    except json.JSONDecodeError:
+        raise HTTPException(status_code=400, detail="Invalid JSON")
+
+
+
+# async def get_event(data):
+#     print(data)
+#     # db = client['iot']['event']
     
-    result = db.insert_one({
-        "sensor_name": sensor_name,
-        "status": status,
-        "value": value,
-        "description": description,
-        "created_at": datetime.now(),
-        "updated_at": datetime.now()
-    })
-    if result.inserted_id:
-        return {"message": "Event created successfully"}
-    else:
-        raise HTTPException(status_code=500, detail="Failed to create event")
+#     # result = db.insert_one({
+#     #     "sensor_name": sensor_name,
+#     #     "status": status,
+#     #     "value": value,
+#     #     "description": description,
+#     #     "created_at": datetime.now(),
+#     #     "updated_at": datetime.now()
+#     # })
+#     if True:
+#     # if result.inserted_id:
+#         return {"message": "Event created successfully"}
+#     else:
+#         raise HTTPException(status_code=500, detail="Failed to create event")
+
+@app.get("/deleteAll")
+
+def deleteAll():
+    client['iot']['event'].delete_many({})
+    return {"message": "All data deleted"}
